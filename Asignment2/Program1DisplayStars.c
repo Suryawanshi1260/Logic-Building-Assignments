38a801724b94b279ddd513cb9ea29eb150e68669
Abhishek Suryawanshi <suryawanshi1260@gmail.com>
2026-05-27 01:21:28 +0530

    Add files via upload

---
+++ Asignment2/Program1DisplayStars.c
@@ -0,0 +1,25 @@
+#include<Stdio.h> 
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    //Write Updater
+    while (iCnt < iNo)
+    {
+       printf("\t*");
+       iCnt++;
+    }
+    
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number:\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+
+}
