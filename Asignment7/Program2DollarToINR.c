a6bfd3fcd3b69af96e98863939dce38144091250
Abhishek Suryawanshi <suryawanshi1260@gmail.com>
2026-05-29 14:14:46 +0530

    Add Program2DollarToINR to convert USD to INR

---
+++ Asignment7/Program2DollarToINR.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int DollarToINR(int iNo)
+
+{
+    int iAmount = 0;
+
+    iAmount = iNo * 70;
+
+    return iAmount;
+
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter a Number of USD :\n");
+    scanf("%d",&iValue);
+
+    iRet=DollarToINR(iValue);
+
+    printf("The value of INR is %d",iRet);
+
+    return 0;
+
+}
