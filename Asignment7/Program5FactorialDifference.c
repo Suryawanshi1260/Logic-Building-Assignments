c8c45f1761afe02bfe90d28fd49643739d86565b
Abhishek Suryawanshi <suryawanshi1260@gmail.com>
2026-05-29 14:17:16 +0530

    Add Program5FactorialDifference.c to calculate difference

---
+++ Asignment7/Program5FactorialDifference.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int FactorialDif(int iNO)
+{
+    int iCnt = 0;
+    int iEvenFact = 1;
+    int iOddFact = 1;
+
+    if(iNO < 0)
+    {
+        iNO = -iNO;
+    }
+
+    for(iCnt = 1; iCnt <= iNO; iCnt++)
+    {
+        if(iCnt % 2 == 0)
+        {
+            iEvenFact = iEvenFact * iCnt;
+        }
+        else
+        {
+            iOddFact = iOddFact * iCnt;
+        }
+    }
+
+    return (iEvenFact - iOddFact);
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter a Number :\n");
+    scanf("%d",&iValue);
+
+    iRet = FactorialDif(iValue);
+
+    printf("The Diffrence between Even Factorial and Odd Factorial is : %d\t",iRet);
+
+    return 0;
+}
